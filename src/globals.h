18bbeba7e31e3edfca0b1e0310b00218097dfc87
toledo <toledo@localhost>
2000-03-18 21:12:24 +0100

    *** empty log message ***

--- src/globals.h
+++ src/globals.h
@@ -29,11 +29,11 @@ gchar *filter;
 GtkWidget *app1;		/* Pointer to the main app window */
 GtkWidget *diag_pref;		/* Pointer to the diagram configuration window */
 
-double node_radius_multiplier;		/* used to calculate the radius of the
-						 * displayed nodes. So that the user can
-						 * select with certain precision this
-						 * value, the GUI uses the log10 of the
-						 * multiplier*/
+double node_radius_multiplier;	/* used to calculate the radius of the
+				   * displayed nodes. So that the user can
+				   * select with certain precision this
+				   * value, the GUI uses the log10 of the
+				   * multiplier */
 double link_width_multiplier;	/* Same explanation as above */
 
 typedef enum
@@ -47,8 +47,8 @@ typedef enum
 size_mode_t;
 
 
-size_mode_t size_mode;	/* Default mode for node size and
-				 * link width calculation */
+size_mode_t size_mode;		/* Default mode for node size and
+				   * link width calculation */
 
 gchar *node_color, *link_color, *text_color;
 gchar *fontname;
@@ -63,20 +63,20 @@ GTree *canvas_links;		/* See above */
 gboolean need_reposition;	/* It is set when a canvas_node has been added 
 				 * or deleted */
 
-double averaging_time;	/* Microseconds of time we consider to
-					 * calculate traffic averages */
+double averaging_time;		/* Microseconds of time we consider to
+				   * calculate traffic averages */
 double link_timeout_time;	/* After this time
-					 * has passed with no traffic in a 
-					 * link, it disappears */
+				   * has passed with no traffic in a 
+				   * link, it disappears */
 double node_timeout_time;	/* After this time has passed 
-					 * with no traffic in/out a 
-					 * node, it disappears */
+				   * with no traffic in/out a 
+				   * node, it disappears */
 struct timeval now;
 
 /* Global variables */
 guint node_id_length;		/* Length of the node_id key. Depends
 				 * on the mode of operation */
-apemode_t mode;	/* Mode of operation. Can be
+apemode_t mode;			/* Mode of operation. Can be
 				   * ETHERNET, IP, UDP or TCP */
 link_type_t linktype;		/* Type of device we are listening to */
 guint l3_offset;		/* Offset to the level 3 protocol data
