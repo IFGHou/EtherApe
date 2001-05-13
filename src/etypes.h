d57a45c6fd35acecfbe69f1cf37c2dd5e2d2af0f
toledo <toledo@localhost>
2001-05-13 20:54:04 +0200

    Added many new ethernet packet types

--- src/etypes.h
+++ src/etypes.h
@@ -46,21 +46,37 @@ ethhdrtype_t;
 typedef enum
   {
     ETHERTYPE_UNK = 0x0000,
+    ETHERTYPE_VINES = 0xbad,
+    ETHERTYPE_TRAIN = 0x1984,
+    ETHERTYPE_3C_NBP_DGRAM = 0x3c07,
     ETHERTYPE_IP = 0x0800,
-    ETHERTYPE_IPv6 = 0x086dd,
-    ETHERTYPE_ARP = 0x0806,
     ETHERTYPE_X25L3 = 0x0805,
+    ETHERTYPE_ARP = 0x0806,
+    ETHERTYPE_DEC = 0x6000,
+    ETHERTYPE_DNA_DL = 0x6001, 
+    ETHERTYPE_DNA_RC = 0x6002,
+    ETHERTYPE_DNA_RT = 0x6003,
+    ETHERTYPE_LAT = 0x6004,
+    ETHERTYPE_DEC_DIAG = 0x6005,
+    ETHERTYPE_DEC_CUST = 0x6006,
+    ETHERTYPE_DEC_SCA = 0x6007,
+    ETHERTYPE_ETHBRIDGE = 0x6558, /* transparent Ethernet bridging */
+    ETHERTYPE_DEC_LB = 0x8038,
+    ETHERTYPE_IPv6 = 0x086dd,
     ETHERTYPE_REVARP = 0x8035,
     ETHERTYPE_ATALK = 0x809b,
     ETHERTYPE_AARP = 0x80f3,
     ETHERTYPE_IPX = 0x8137,
-    ETHERTYPE_VINES = 0xbad,
-    ETHERTYPE_TRAIN = 0x1984,
-    ETHERTYPE_LOOP = 0x9000,	/* used for layer 2 testing (do i see my own frames on the wire) */
+    ETHERTYPE_VLAN = 0x8100,  /* 802.1Q Virtual LAN */
+    ETHERTYPE_SNMP = 0x814c,  /* SNMP over Ethernet, RFC 1089 */
+    ETHERTYPE_WCP = 0x80ff, /* Wellfleet Compression Protocol */
+    ETHERTYPE_IPv6 = 0x86dd,
+    ETHERTYPE_PPP = 0x880b, /* no, this is not PPPoE */
+    ETHERTYPE_MPLS = 0x8847, /* MPLS unicast packet */
+    ETHERTYPE_MPLS_MULTI = 0x8848, /* MPLS multicast packet */
     ETHERTYPE_PPPOED = 0x8863,	/* PPPoE Discovery Protocol */
     ETHERTYPE_PPPOES = 0x8864,	/* PPPoE Session Protocol */
-    ETHERTYPE_VLAN = 0x8100,	/* 802.1Q Virtual LAN */
-    ETHERTYPE_SNMP = 0x814c	/* SNMP over Ethernet, RFC 1089 */
+    ETHERTYPE_LOOP = 0x9000,	/* used for layer 2 testing (do i see my own frames on the wire) */
   }
 etype_t;
 #endif /* etypes.h */
