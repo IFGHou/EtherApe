eedeb7cdacd595efc854c11625363cc939630889
R.Ghetta <bchiara@users.sf.net>
2009-10-07 21:50:11 +0200

    update autogen and spec

--- autogen.sh
+++ autogen.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
-
-PKG_NAME="etherape"
-
-(test -f $srcdir/configure.in) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level directory"
-    exit 1
-}
-
 autoreconf -fvis
 #intltoolize  --automake
-autoconf 
